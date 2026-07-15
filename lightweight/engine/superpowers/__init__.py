14164ba091e3894f6c7b8dc146a7fced2c95a550
err0rgod <ndgaming458@gmail.com>
2026-07-16 04:19:59 +0530

    chore: fully remove legacy lightweight code

--- lightweight/engine/superpowers/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from .create_file import create_file
-from .execute_command import execute_command
-from .read_url import read_url
-from .save_memory import save_memory
-from .search_web import search_web
-
-__all__ = ["create_file", "execute_command", "read_url", "save_memory", "search_web"]
